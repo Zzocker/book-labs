1a81fab9ef7c444ae737ef31d24a4df04104e4ae
Pritam Singh <pkspritam16@gmail.com>
2021-10-01 01:35:55 +0530

    pkg(errors): add http status code mapping of code
    Signed-off-by: Pritam Singh <pkspritam16@gmail.com>

--- pkg/errors/code.go
+++ pkg/errors/code.go
@@ -1,6 +1,8 @@
 package errors
 
 import (
+	"net/http"
+
 	"google.golang.org/grpc/codes"
 )
 
@@ -16,3 +18,20 @@ const (
 	CodeUnexpected    Code = Code(codes.Internal)
 	CodeAlreadyExists Code = Code(codes.AlreadyExists)
 )
+
+func ErrCodeToHTTP(c Code) int {
+	switch c {
+	case CodeNotFound:
+		return http.StatusNotFound
+	case CodeUnauthorized:
+		return http.StatusUnauthorized
+	case CodeInvalidInput:
+		return http.StatusBadRequest
+	case CodeUnexpected:
+		return http.StatusInternalServerError
+	case CodeAlreadyExists:
+		return http.StatusConflict
+	default:
+		return http.StatusInternalServerError
+	}
+}
